781cce32d6dcb7636ec9d71009717dddc53d4c85
Ogwen123 <owen.d.jones@btinternet.com>
2025-03-14 16:02:55 +0000

    command parsing, im stupid and this took way too long

---
+++ README.md
@@ -0,0 +1,8 @@
+# runtime
+program to find the average runtime of a command.
+## Usage
+```shell
+runtime "<command>" <options>
+```
+### Flags
+```-runs <run count>``` - Set the number of times the command is run, the default is 5.
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,47 @@
 mod utils;
 
-use crate::utils::logger::{fatal, info, warning};
+use crate::utils::logger::{fatal, warning};
 use std::env;
 
 pub struct Config {
-    runs: u32
+    runs: u32,
+    command: String
 }
 
 fn main() {
     let mut config: Config = Config {
-        runs: 5
+        runs: 5,
+        command: String::new()
     };
 
     let args: Vec<String> = env::args().collect();
 
-    let runs_flag: String = "-runs".to_string();
+    if args.len() == 1 {
+        warning!("You must provide a command to run between quotes. E.g. runtime \"<command>\"")
+    }
 
+    let runs_flag: String = String::from("-runs");
+
+    // get command from args
+    let command_res = args.get(1);
+
+    config.command = match command_res {
+        Some(command) => command.clone(),
+        None => {
+            warning!("No command supplied");
+            return;
+        }
+    };
+
+    // get run count from args
     if args.contains(&runs_flag) {
         let pos: usize = args.iter().position(|x| x == "-runs").unwrap();
 
         let item_result: Option<&String> = args.get(pos + 1);
 
-        let res = match item_result {
-            Ok(str) => str,
-            Err(_) => {
+        let res: &String = match item_result {
+            Some(item) => item,
+            None => {
                 fatal!("The -runs flags should be used as follows: ... -run <run count> ...");
                 return;
             }
@@ -33,9 +51,10 @@ fn main() {
             Ok(num) => num,
             Err(_) => {
                 fatal!("The -runs flags should be used as follows: ... -run <number> ...");
+                return;
             }
         };
 
-        config.runs = parse_result;
+        config.runs = parse_result as u32;
     }
 }
